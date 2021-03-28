2bbbfb9fd954053612990f93d5604bd0b6499ed0
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2021-03-28 21:11:21 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,21 @@
+# puts "Hello World\n"
+# print "Hello World\n"
+# p "Hello World\n"
+
+# spice_rack = [
+#   ["Mace", "Ginger", "Marojam"],          
+#   ["Paprika", "Fajita Mix", "Coriander"], 
+#   ["Parsley", "Sage", "Rosemary"]         
+# ]
+
+given_nums = [0, 1, 0, 3, 12]
+
+def move_zeroes(nums)
+  
+end
+
+def hello 
+  puts "hello"
+end
+
+  
